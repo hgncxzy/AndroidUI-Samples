a57186861d2fbf8a85bffb28d9c14b9b9d4b71ba
许助云 <xuzhuyun@fcbox.com>
2019-08-13 19:56:17 +0800

    feat: 添加 edittext 库

--- edittext/README.md
+++ edittext/README.md
@@ -1,6 +1,6 @@
-## 第一种  EditText 效果库的使用
+### [使用库1](https://github.com/liujinchao/XSelectorUtil)
 
-### 使用库1
+一个可以用代码处理控件的阴影效果，及用代码在TextView、EditText、Button等控件设置selector背景（触摸反馈，样式变化、文字颜色变化、hint文字颜色变化等效果）的组件
 
 #### 库名
 
@@ -26,3 +26,8 @@ com.android.util:xselector:1.0.2
 ####  APIs 与调用方式
 
 参考 https://github.com/liujinchao/XSelectorUtil
+
+### [使用库2](https://github.com/Carson-Ho/SuperEditText)
+
+一款简单、好用的自定义EditText（含一键删除&自定义样式）
+
--- edittext/src/main/java/com/xzy/edittext/EditTextActivity.java
+++ edittext/src/main/java/com/xzy/edittext/EditTextActivity.java
@@ -9,21 +9,29 @@ import android.os.Bundle;
  *
  * @author xzy
  */
+@SuppressWarnings("unused")
 public class EditTextActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_text);
-        handle();
     }
 
     /**
-     * 依赖包 com.android.util:xselector:1.0.2
+     * 一个可以用代码处理控件的阴影效果，及用代码在TextView、EditText、Button等控件设置selector背景
+     * （触摸反馈，样式变化、文字颜色变化、hint文字颜色变化等效果）的组件
      * 依赖库地址 https://github.com/liujinchao/XSelectorUtil
-     * 用法参考 https://github.com/liujinchao/XSelectorUtil
      */
-    private void handle(){
+    private void handle() {
+
+    }
+
+    /**
+     * 一款简单、好用的自定义EditText（含一键删除&自定义样式）
+     * 依赖库地址 https://github.com/Carson-Ho/SuperEditText
+     */
+    private void handle2() {
 
     }
 }
