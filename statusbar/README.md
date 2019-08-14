ce207c446387eb1c8041daa608aa760487a530ed
许助云 <xuzhuyun@fcbox.com>
2019-08-14 12:43:09 +0800

    feat: 添加 statusbar 库。

--- statusbar/README.md
+++ statusbar/README.md
@@ -2,3 +2,7 @@
 
 android 4.4 以上沉浸式状态栏和沉浸式导航栏管理，适配横竖屏切换、刘海屏、软键盘弹出等问题，可以修改状态栏字体颜色和导航栏图标颜色，以及不可修改字体颜色手机的适配，适用于Activity、Fragment、DialogFragment、Dialog，PopupWindow，一句代码轻松实现，以及对bar的其他设置，详见README。简书请参考：<http://www.jianshu.com/p/2a884e211a62>
 
+## [使用库2](https://github.com/Ye-Miao/StatusBarUtil)
+
+Android沉浸式状态栏，支持状态栏渐变色，纯色， 全屏，亮光、暗色模式，适配android 4.4 -10.0机型
+
--- statusbar/src/main/java/com/xzy/statusbar/MainActivity.java
+++ statusbar/src/main/java/com/xzy/statusbar/MainActivity.java
@@ -23,10 +23,18 @@ public class MainActivity extends AppCompatActivity {
      * 可以修改状态栏字体颜色和导航栏图标颜色，以及不可修改字体颜色手机的适配，
      * 适用于Activity、Fragment、DialogFragment、Dialog，PopupWindow，
      * 一句代码轻松实现，以及对bar的其他设置
-     *
-     *依赖库地址 https://github.com/gyf-dev/ImmersionBar
+     * <p>
+     * 依赖库地址 https://github.com/gyf-dev/ImmersionBar
      */
-    private void handle(){
+    private void handle() {
+
+    }
+
+    /**
+     * Android沉浸式状态栏，支持状态栏渐变色，纯色， 全屏，亮光、暗色模式，适配android 4.4 -10.0机型
+     * 依赖库地址 https://github.com/Ye-Miao/StatusBarUtil
+     */
+    private void handle2() {
 
     }
 }
